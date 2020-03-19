f121abfb75bc51f5ac7e446037e755e333ff331c
YEISON CIFUENTES <yeisoncifuentes2@gmail.com>
2020-03-19 13:24:42 -0500

    Impresion de la carrera en orden

--- CarreraHilos/src/edu/ucundinamarca/logica/ImprimirCorredores.java
+++ CarreraHilos/src/edu/ucundinamarca/logica/ImprimirCorredores.java
@@ -17,7 +17,9 @@ public class ImprimirCorredores extends Thread {
 
     private boolean inicioCarrera = true;
     private List<Corredor> corredores;
-    
+    private StringBuffer pasosEquipo1 = new StringBuffer();
+    private StringBuffer pasosEquipo2 = new StringBuffer();
+    private StringBuffer pasosEquipo3 = new StringBuffer();
 
     public ImprimirCorredores(List<Corredor> corredores) {
         this.corredores = corredores;
@@ -25,11 +27,15 @@ public class ImprimirCorredores extends Thread {
 
     @Override
     public void run() {
-        
+
         if (inicioCarrera) {
             try {
                 //Permite esperar que empiecen los corredores antes de imprimir sus posiciones
                 sleep(10);
+               
+                         
+                          
+
             } catch (InterruptedException ex) {
                 Logger.getLogger(ImprimirCorredores.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -45,47 +51,73 @@ public class ImprimirCorredores extends Thread {
                 || corredores.get(7).getPosicionActual() <= corredores.get(7).getPosicionFinal()
                 || corredores.get(8).getPosicionActual() <= corredores.get(8).getPosicionFinal()) {
 
+            System.out.println("---------------------------");
+
             if (corredores.get(0).getPosicionActual() <= corredores.get(0).getPosicionFinal()
                     && corredores.get(0).getPosicionActual() != corredores.get(0).getPosicionInicio()) {
-                System.out.print(corredores.get(0).getPaso());
+                pasosEquipo1.append(corredores.get(0).getPaso());
+                System.out.println(pasosEquipo1);
             }
 
             if (corredores.get(1).getPosicionActual() <= corredores.get(1).getPosicionFinal()
                     && corredores.get(1).getPosicionActual() != corredores.get(1).getPosicionInicio()) {
-                System.out.print(corredores.get(1).getPaso());
+                pasosEquipo1.append(corredores.get(1).getPaso());
+                System.out.println(pasosEquipo1);
             }
             if (corredores.get(2).getPosicionActual() <= corredores.get(2).getPosicionFinal()
                     && corredores.get(2).getPosicionActual() != corredores.get(2).getPosicionInicio()) {
-                System.out.print(corredores.get(2).getPaso());
+                pasosEquipo1.append(corredores.get(2).getPaso());
+                System.out.println(pasosEquipo1);
             }
-            
+
+            if (corredores.get(2).getPosicionActual() - 4 == corredores.get(2).getPosicionFinal()) {
+                System.out.println(pasosEquipo1);
+            }
+
             if (corredores.get(3).getPosicionActual() <= corredores.get(3).getPosicionFinal()
                     && corredores.get(3).getPosicionActual() != corredores.get(3).getPosicionInicio()) {
-                System.out.print(corredores.get(3).getPaso());
+                pasosEquipo2.append(corredores.get(3).getPaso());
+                System.out.println(pasosEquipo2);
             }
-            
+
             if (corredores.get(4).getPosicionActual() <= corredores.get(4).getPosicionFinal()
                     && corredores.get(4).getPosicionActual() != corredores.get(4).getPosicionInicio()) {
-                System.out.print(corredores.get(4).getPaso());
+                pasosEquipo2.append(corredores.get(4).getPaso());
+                System.out.println(pasosEquipo2);
             }
             if (corredores.get(5).getPosicionActual() <= corredores.get(5).getPosicionFinal()
                     && corredores.get(5).getPosicionActual() != corredores.get(5).getPosicionInicio()) {
-                System.out.print(corredores.get(5).getPaso());
+                pasosEquipo2.append(corredores.get(5).getPaso());
+                System.out.println(pasosEquipo2);
             }
-            
+
+            if (corredores.get(5).getPosicionActual() - 4 == corredores.get(5).getPosicionFinal()) {
+                System.out.println(pasosEquipo2);
+            }
+
             if (corredores.get(6).getPosicionActual() <= corredores.get(6).getPosicionFinal()
                     && corredores.get(6).getPosicionActual() != corredores.get(6).getPosicionInicio()) {
-                System.out.print(corredores.get(6).getPaso());
+                pasosEquipo3.append(corredores.get(6).getPaso());
+                System.out.println(pasosEquipo3);
             }
-           
+
             if (corredores.get(7).getPosicionActual() <= corredores.get(7).getPosicionFinal()
                     && corredores.get(7).getPosicionActual() != corredores.get(7).getPosicionInicio()) {
-                System.out.print(corredores.get(7).getPaso());
+                pasosEquipo3.append(corredores.get(7).getPaso());
+                System.out.println(pasosEquipo3);
             }
             if (corredores.get(8).getPosicionActual() <= corredores.get(8).getPosicionFinal()
                     && corredores.get(8).getPosicionActual() != corredores.get(8).getPosicionInicio()) {
-                System.out.print(corredores.get(8).getPaso());
+                pasosEquipo3.append(corredores.get(8).getPaso());
+                System.out.println(pasosEquipo3);
             }
+
+            if (corredores.get(8).getPosicionActual() - 4 == corredores.get(8).getPosicionFinal()) {
+                System.out.println(pasosEquipo3);
+            }
+            System.out.println("---------------------------");
+            System.out.println("");
+            System.out.println("");
 
             try {
                 sleep(1000);
